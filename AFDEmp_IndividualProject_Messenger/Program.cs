6aeecb6216cbf92735859a03cfb7dcc710b107d5
Kiriakos Makris <Kiriakos.Makris@Peoplecert.onmicrosoft.com>
2018-12-15 20:57:21 +0200

    Add project files.

---
+++ AFDEmp_IndividualProject_Messenger/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFDEmp_IndividualProject_Messenger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
